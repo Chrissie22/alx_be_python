03d891b682d6f5968d8e7da7c2845ec53caae92f
Chrissie22 <christabel.ojobolo@gmail.com>
2024-06-21 13:08:43 +0100

    calculate the simple interest of the numbers in the file simple_interst.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
-principal = 1000
-rate = 0.05
-time = 3
-Interest = principal*rate*time
+Principal = 1000
+Rate = 0.05
+Time = 3
+Interest = Principal*Rate*Time
 
 print(f" The simple interest is: {Interest}")
