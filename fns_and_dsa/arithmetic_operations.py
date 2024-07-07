a41aa9a8a72b337d6c11d1b4f4a836c6879291d7
Chrissie22 <christabel.ojobolo@gmail.com>
2024-07-07 19:10:24 +0100

    A function that performs basic arithmetic operations. This function, perform_operation, will be imported and used in a separate main.py script, which is  provide.

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
         return num1 * num2
     elif operation == "divide":
         if num2 == "0":
-            print("Cannot divide by zero")
+            raise ValueError("Cannot divide by zero")
         else:
             return num1 / num2
     else:
