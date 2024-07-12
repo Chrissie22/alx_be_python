498969c0804bbbee1ffac1d4586ead1d9230bcba
Chrissie22 <christabel.ojobolo@gmail.com>
2024-07-12 10:14:26 +0100

    A simple shopping list manager that allows users to add items, view the current list, and remove items

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
         
         if choice == '1':
-            item = input("Add an item: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} has been added")
         elif choice == '2':
