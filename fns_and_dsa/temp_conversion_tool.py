3d48544084e20c338b0df28e9741e7657f73708d
Chrissie22 <christabel.ojobolo@gmail.com>
2024-07-12 17:08:33 +0100

    converts temperatures between Celsius and Fahrenheit, using global variables to store conversion factors

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,13 +1,10 @@
-"""
-    Reads temperature as input and operates convertion between celcius and ferenheit 
-"""
-
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
 def convert_to_celsius(fahrenheit):
     celcius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celcius
 
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 def convert_to_fahrenheit(celsius):
     farenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return farenheit
